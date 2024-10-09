d6798f2bbab154dc2d204745f1add06cbcef5040
puppygirlhornypost <puppygirlhornyposting@gmail.com>
2024-10-09 15:50:05 +0000

    fix some missing endpoint validations

---
+++ packages/backend/src/server/api/endpoints/promo/read.test.ts
@@ -0,0 +1,46 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+process.env.NODE_ENV = 'test';
+
+import { describe, test, expect } from '@jest/globals';
+import { getValidator } from '../../../../../test/prelude/get-api-validator.js';
+import { paramDef } from './read.js';
+
+const VALID = true;
+const INVALID = false;
+
+describe('api:promo/read', () => {
+	describe('validation', () => {
+		const v = getValidator(paramDef);
+
+		test('reject empty', () => {
+			const valid = v({ });
+			expect(valid).toBe(INVALID);
+		});
+
+		describe('noteId', () => {
+			test('accept id', () => {
+				expect(v({ noteId: 'x' }))
+					.toBe(VALID);
+			});
+
+			test('null id', () => {
+				expect(v({ noteId: null }))
+					.toBe(INVALID);
+			});
+
+			test('0 character id', () => {
+				expect(v({ noteId: '' }))
+					.toBe(INVALID);
+			});
+
+			test('whitespace-only id', () => {
+				expect(v({ noteId: ' ' }))
+					.toBe(INVALID);
+			});
+		});
+	});
+});
