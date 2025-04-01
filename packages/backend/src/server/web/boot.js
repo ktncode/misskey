d272d6f224eee620c261cb1af4b4a82ffb0d1de4
Hazelnoot <acomputerdog@gmail.com>
2025-04-01 12:29:57 -0400

    fix software name in boot.embed.js,  boot.js, and error.js

--- packages/backend/src/server/web/boot.js
+++ packages/backend/src/server/web/boot.js
@@ -164,7 +164,7 @@
 		const locale = JSON.parse(localStorage.getItem('locale') || '{}');
 
 		const messages = Object.assign({
-			title: 'Failed to initialize Misskey',
+			title: 'Failed to initialize Sharkey',
 			solution: 'The following actions may solve the problem.',
 			solution1: 'Update your os and browser',
 			solution2: 'Disable an adblocker',
