c866c16ed3aabcc9edd9436392e876a81e40a7a8
Hazelnoot <acomputerdog@gmail.com>
2025-04-13 19:59:16 -0400

    remove duplicate button in MkDrive.vue

--- packages/frontend/src/components/MkDrive.vue
+++ packages/frontend/src/components/MkDrive.vue
@@ -31,7 +31,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 				<span v-if="folder != null" :class="[$style.navPathItem, $style.navSeparator]"><i class="ti ti-chevron-right"></i></span>
 				<span v-if="folder != null" :class="[$style.navPathItem, $style.navCurrent]">{{ folder.name }}</span>
 			</div>
-			<button class="_button" :class="$style.navMenu" @click="showMenu"><i class="ti ti-dots"></i></button>
 			<div :class="$style.navMenu">
 				<!-- "Search drive via alt text or file names" -->
 				<MkInput v-model="searchQuery" :large="true" :autofocus="true" type="search" :placeholder="i18n.ts.driveSearchbarPlaceholder" @enter="fetch">
