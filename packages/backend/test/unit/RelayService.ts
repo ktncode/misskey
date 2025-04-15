885fae0693c3a31b288ee28afe77b1067c44be5a
Hazelnoot <acomputerdog@gmail.com>
2025-04-15 11:28:28 -0400

    add unit tests for deliverToRelays

--- packages/backend/test/unit/RelayService.ts
+++ packages/backend/test/unit/RelayService.ts
@@ -5,11 +5,12 @@
 
 process.env.NODE_ENV = 'test';
 
-import { jest } from '@jest/globals';
+import { expect, jest } from '@jest/globals';
 import { Test } from '@nestjs/testing';
 import { ModuleMocker } from 'jest-mock';
 import type { TestingModule } from '@nestjs/testing';
 import type { MockFunctionMetadata } from 'jest-mock';
+import type { IActivity } from '@/core/activitypub/type.js';
 import { ApRendererService } from '@/core/activitypub/ApRendererService.js';
 import { UserEntityService } from '@/core/entities/UserEntityService.js';
 import { IdService } from '@/core/IdService.js';
@@ -21,6 +22,8 @@ import { UtilityService } from '@/core/UtilityService.js';
 import { UserFollowingService } from '@/core/UserFollowingService.js';
 import { RelaysRepository } from '@/models/_.js';
 import { DI } from '@/di-symbols.js';
+import { UserKeypairService } from '@/core/UserKeypairService.js';
+import { JsonLdService } from '@/core/activitypub/JsonLdService.js';
 
 const moduleMocker = new ModuleMocker(global);
 
@@ -30,6 +33,8 @@ describe('RelayService', () => {
 	let queueService: jest.Mocked<QueueService>;
 	let relaysRepository: RelaysRepository;
 	let idService: IdService;
+	let systemAccountService: SystemAccountService;
+	let apRendererService: ApRendererService;
 
 	beforeAll(async () => {
 		app = await Test.createTestingModule({
@@ -44,6 +49,8 @@ describe('RelayService', () => {
 				SystemAccountService,
 				UtilityService,
 				UserFollowingService,
+				UserKeypairService,
+				JsonLdService,
 				{
 					provide: 'UserFollowingService',
 					useClass: UserFollowingService,
@@ -69,10 +76,17 @@ describe('RelayService', () => {
 		queueService = app.get<QueueService>(QueueService) as jest.Mocked<QueueService>;
 		relaysRepository = app.get<RelaysRepository>(DI.relaysRepository);
 		idService = app.get<IdService>(IdService);
+		systemAccountService = app.get<SystemAccountService>(SystemAccountService);
+		apRendererService = app.get<ApRendererService>(ApRendererService);
 	});
 
 	afterAll(async () => {
 		await app.close();
+	});
+
+	afterEach(() => {
+		// Make sure the mock counters reset
+		queueService.deliver.mockClear();
 	});
 
 	test('addMastodonRelay should update status and deliver activity', async () => {
@@ -150,4 +164,130 @@ describe('RelayService', () => {
 			await relaysRepository.delete({ id });
 		}
 	});
+
+	test('deliverToRelays should bypass on null input', async () => {
+		const user = await systemAccountService.getInstanceActor();
+		const activity = null;
+
+		await relayService.deliverToRelays(user, activity);
+
+		expect(queueService.deliver).not.toHaveBeenCalled();
+	});
+
+	test('deliverToRelays should bypass if there are no relays', async () => {
+		const user = await systemAccountService.getInstanceActor();
+		const activity = {} as unknown as IActivity;
+
+		await relayService.deliverToRelays(user, activity);
+
+		expect(queueService.deliver).not.toHaveBeenCalled();
+	});
+
+	test('deliverToRelays should deliver to all relays', async () => {
+		const user = await systemAccountService.getInstanceActor();
+		const activity = apRendererService.addContext<IActivity>({
+			type: 'Announce',
+			id: 'https://example.com/activity',
+			actor: 'https://example.com/actor',
+			object: 'https://example.com/object',
+		});
+		const relays = [
+			'https://relay1.example.com/inbox',
+			'https://relay2.example.com/inbox',
+		];
+
+		try {
+			await createRelays(relays);
+			await relayService.deliverToRelays(user, activity);
+		} finally {
+			await deleteRelays(relays);
+		}
+
+		for (const relay of relays) {
+			expect(queueService.deliver).toHaveBeenCalledWith(
+				user,
+				expect.objectContaining({}),
+				relay,
+				false,
+			);
+		}
+	});
+
+	test('deliverToRelays should sign activities', async () => {
+		const user = await systemAccountService.getInstanceActor();
+		const activity = apRendererService.addContext<IActivity>({
+			type: 'Announce',
+			id: 'https://example.com/activity',
+			actor: 'https://example.com/actor',
+			object: 'https://example.com/object',
+		});
+		const relays = [
+			'https://relay1.example.com/inbox',
+			'https://relay2.example.com/inbox',
+		];
+
+		try {
+			await createRelays(relays);
+			await relayService.deliverToRelays(user, activity);
+		} finally {
+			await deleteRelays(relays);
+		}
+
+		for (const relay of relays) {
+			expect(queueService.deliver).toHaveBeenCalledWith(
+				expect.anything(),
+				expect.objectContaining({
+					signature: expect.anything(),
+				}),
+				relay,
+				expect.anything(),
+			);
+		}
+	});
+
+	test('deliverToRelays should populate audience', async () => {
+		const user = await systemAccountService.getInstanceActor();
+		const activity = apRendererService.addContext<IActivity>({
+			type: 'Announce',
+			id: 'https://example.com/activity',
+			actor: 'https://example.com/actor',
+			object: 'https://example.com/object',
+		});
+		const relays = [
+			'https://relay1.example.com/inbox',
+			'https://relay2.example.com/inbox',
+		];
+
+		try {
+			await createRelays(relays);
+			await relayService.deliverToRelays(user, activity);
+		} finally {
+			await deleteRelays(relays);
+		}
+
+		for (const relay of relays) {
+			expect(queueService.deliver).toHaveBeenCalledWith(
+				expect.anything(),
+				expect.objectContaining({
+					to: ['https://www.w3.org/ns/activitystreams#Public'],
+				}),
+				relay,
+				false,
+			);
+		}
+	});
+
+	async function createRelays(relays: string[]) {
+		for (const relay of relays) {
+			const { id } = await relayService.addMastodonRelay(relay);
+			await relayService.acceptMastodonRelay(id);
+		}
+		queueService.deliver.mockClear();
+	}
+
+	async function deleteRelays(relays: string[]) {
+		for (const relay of relays) {
+			await relayService.removeMastodonRelay(relay);
+		}
+	}
 });
