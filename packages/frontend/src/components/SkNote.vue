d41372f948285de535309339b7230a64f86c41a9
dakkar <dakkar@thenautilus.net>
2025-05-03 08:39:49 +0100

    (manually) revert bc422fe36ff661d83d01cce17225a31d67829bd5
    the fix for #1037 was wrong (it would collapse a boosted quote if the
    quoted note was from the current user), and not
    necessary (`appearNote.value.userId` _is_ the author of the boosted
    note, no need to look inside `note.renote` at all)
    
    so… I'm not sure why I even raised the issue 😕

--- packages/frontend/src/components/SkNote.vue
+++ packages/frontend/src/components/SkNote.vue
@@ -343,8 +343,7 @@ const showTicker = (prefer.s.instanceTicker === 'always') || (prefer.s.instanceT
 const canRenote = computed(() => ['public', 'home'].includes(appearNote.value.visibility) || (appearNote.value.visibility === 'followers' && appearNote.value.userId === $i?.id));
 const renoteCollapsed = ref(
 	prefer.s.collapseRenotes && isRenote && (
-		($i && ($i.id === note.value.userId || $i.id === appearNote.value.userId
-				 || $i.id === note.value.renote?.userId || $i.id === appearNote.value.renote?.userId )) || // `||` must be `||`! See https://github.com/misskey-dev/misskey/issues/13131
+		($i && ($i.id === note.value.userId || $i.id === appearNote.value.userId)) || // `||` must be `||`! See https://github.com/misskey-dev/misskey/issues/13131
 		(appearNote.value.myReaction != null)
 	),
 );
