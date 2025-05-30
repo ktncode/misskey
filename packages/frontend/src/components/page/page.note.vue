85bc401e471454c82c43093926ae2ba16fa93088
Outvi V <git@outv.im>
2025-05-31 00:40:13 +0800

    feat(page.note): add delay and retry to embedded note loading

--- packages/frontend/src/components/page/page.note.vue
+++ packages/frontend/src/components/page/page.note.vue
@@ -11,7 +11,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 import * as Misskey from 'misskey-js';
 import MkNote from '@/components/MkNote.vue';
 import MkNoteDetailed from '@/components/MkNoteDetailed.vue';
@@ -20,16 +20,57 @@ import { misskeyApi } from '@/utility/misskey-api.js';
 const props = defineProps<{
 	block: Misskey.entities.PageBlock,
 	page: Misskey.entities.Page,
+	index: number;
 }>();
 
 const note = ref<Misskey.entities.Note | null>(null);
 
+let timeoutId = null;
+
+async function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => {
+	    setTimeout(() => {
+		    resolve()
+		}, ms);
+	});
+}
+
+async function retryOnThrottle<T>(f: ()=>Promise<T>, retryCount: number = 5): Promise<T> {
+    let lastResult: T = undefined!;
+	const r = Math.random();
+	for(let i=0; i<retryCount; i++) {
+		const [ok, resultOrError] = await f()
+			.then(result => [true, result])
+			.catch(err => [false, err]);
+        
+        lastResult = resultOrError;
+
+		if (ok) {
+			break;
+		}
+
+		// RATE_LIMIT_EXCEEDED
+		if (resultOrError?.id === "d5826d14-3982-4d2e-8011-b9e9f02499ef") {
+			await sleep(resultOrError?.info?.fullResetMs ?? 1000);
+			continue;
+		}
+
+		throw resultOrError;
+	}
+    return lastResult;
+}
+
 onMounted(() => {
 	if (props.block.note == null) return;
-	misskeyApi('notes/show', { noteId: props.block.note })
-		.then(result => {
-			note.value = result;
+	timeoutId = setTimeout(() => {
+		retryOnThrottle(() => misskeyApi('notes/show', { noteId: props.block.note })).then(result => {
+		    note.value = result;
 		});
+	}, 500 * props.index); // rate limit is 2 reqs per sec
+});
+
+onUnmounted(() => {
+    if (timeoutId !== null) clearTimeout(timeoutId);
 });
 </script>
 
