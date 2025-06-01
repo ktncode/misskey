3f77cca7af76191cfd31694fc96199030cb18709
Outvi V <git@outv.im>
2025-06-01 13:20:19 +0800

    chore: lint

--- packages/frontend-shared/js/retry-on-throttled.ts
+++ packages/frontend-shared/js/retry-on-throttled.ts
@@ -13,9 +13,9 @@ export async function retryOnThrottled<T>(f: () => Promise<T>, retryCount = 5): 
 	  return await f();
 	} catch (err) {
 	  // RATE_LIMIT_EXCEEDED
-	  if (err?.id === 'd5826d14-3982-4d2e-8011-b9e9f02499ef') {
+	  if ((err as T)?.id === 'd5826d14-3982-4d2e-8011-b9e9f02499ef') {
 		lastError = err;
-		await sleep(err?.info?.fullResetMs ?? 1000);
+		await sleep((err as T)?.info?.fullResetMs ?? 1000);
 	  } else {
 		throw err;
 	  }
