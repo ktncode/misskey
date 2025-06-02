0e16a56086a45263f812dc5c1ad39449f209ccd4
Hazelnoot <acomputerdog@gmail.com>
2025-06-02 17:26:58 -0400

    remove unused DI from bubble-timeline.ts

--- packages/backend/src/server/api/endpoints/notes/bubble-timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/bubble-timeline.ts
@@ -5,7 +5,7 @@
 
 import { Inject, Injectable } from '@nestjs/common';
 import { Brackets } from 'typeorm';
-import type { NotesRepository, MiMeta } from '@/models/_.js';
+import type { NotesRepository } from '@/models/_.js';
 import { Endpoint } from '@/server/api/endpoint-base.js';
 import { QueryService } from '@/core/QueryService.js';
 import { NoteEntityService } from '@/core/entities/NoteEntityService.js';
@@ -61,9 +61,6 @@ export const paramDef = {
 @Injectable()
 export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-disable-line import/no-default-export
 	constructor(
-		@Inject(DI.meta)
-		private serverSettings: MiMeta,
-
 		@Inject(DI.notesRepository)
 		private notesRepository: NotesRepository,
 
