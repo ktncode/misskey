078399997446b2435335ca0b93730557798942bd
Hazelnoot <acomputerdog@gmail.com>
2025-06-02 17:22:41 -0400

    add copyright header to bubble-timeline.ts

--- packages/backend/src/server/api/endpoints/notes/bubble-timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/bubble-timeline.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: Marie and other Sharkey contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { Inject, Injectable } from '@nestjs/common';
 import { Brackets } from 'typeorm';
 import type { NotesRepository, MiMeta } from '@/models/_.js';
