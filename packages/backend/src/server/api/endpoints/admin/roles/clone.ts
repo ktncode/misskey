18a979ea4effb80aa626b774f7d7f9670de26525
Lilly Schramm <privat@eps-dev.de>
2025-06-20 21:32:05 +0200

    refactor: Add Licence Header

--- packages/backend/src/server/api/endpoints/admin/roles/clone.ts
+++ packages/backend/src/server/api/endpoints/admin/roles/clone.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { Inject, Injectable } from '@nestjs/common';
 import { RoleService } from '@/core/RoleService.js';
 import { DI } from '@/di-symbols.js';
