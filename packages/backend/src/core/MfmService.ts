2680b759020f009d725c65180520188315bff24a
piuvas <mail@piuvas.net>
2025-06-21 21:47:25 -0300

    fix typo and lint

--- packages/backend/src/core/MfmService.ts
+++ packages/backend/src/core/MfmService.ts
@@ -345,7 +345,7 @@ export class MfmService {
 	}
 
 	@bindThis
-	public toHtml(nodes: mfm.MfmNode[] | null, mentionedRemoteUsers: IMentionedRemoteUsers = [], additionalAppenders: Appender[] = [], addRelMe: bool = false) {
+	public toHtml(nodes: mfm.MfmNode[] | null, mentionedRemoteUsers: IMentionedRemoteUsers = [], additionalAppenders: Appender[] = [], addRelMe: boolean = false) {
 		if (nodes == null) {
 			return null;
 		}
@@ -565,7 +565,7 @@ export class MfmService {
 				});
 				// when populating the field links in our ap actors, we want to add rel="me" so mastodon is happy.
 				if (addRelMe) {
-					a.attribs = {...a.attribs, rel: "me",}
+					a.attribs = { ...a.attribs, rel: "me", };
 				}
 				a.childNodes.push(new Text(node.props.url));
 				return a;
