a3fd9b45f450a7f061a7e653fb8ca65511e6c894
Kotone <git@ktn.works>
2025-06-27 20:02:12 +0900

    Fix: Misskey Assets

--- packages/frontend/src/pages/about-sharkey.vue
+++ packages/frontend/src/pages/about-sharkey.vue
@@ -12,7 +12,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 				<div v-panel class="about">
 					<div ref="containerEl" class="container" :class="{ playing: easterEggEngine != null }">
 						<img src="/client-assets/about-icon.png" alt="" class="icon" draggable="false" @load="iconLoaded" @click="gravity"/>
-						<div class="misskey">Sharkey</div>
+						<div class="misskey">Misskey</div>
 						<div class="version">v{{ version }}</div>
 						<span v-for="emoji in easterEggEmojis" :key="emoji.id" class="emoji" :data-physics-x="emoji.left" :data-physics-y="emoji.top" :class="{ _physics_circle_: !emoji.emoji.startsWith(':') }">
 							<MkCustomEmoji v-if="emoji.emoji[0] === ':'" class="emoji" :name="emoji.emoji" :normal="true" :noStyle="true" :fallbackToImage="true"/>
@@ -27,7 +27,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 				<div v-if="$i != null" style="text-align: center;">
 					<MkButton primary rounded inline @click="iLoveMisskey">I <Mfm text="$[jelly ❤]"/> #Sharkey</MkButton>
 				</div>
-				<FormSection v-if="instance.repositoryUrl !== 'https://activitypub.software/TransFem-org/Sharkey/'">
+				<FormSection v-if="instance.repositoryUrl !== 'https://github.com/ktncode/misskey/'">
 					<div class="_gaps_s">
 						<MkInfo>
 							{{ i18n.tsx._aboutMisskey.thisIsModifiedVersion({ name: instance.name ?? '' }) }}
@@ -48,15 +48,10 @@ SPDX-License-Identifier: AGPL-3.0-only
 				</FormSection>
 				<FormSection>
 					<div class="_gaps_s">
-						<FormLink to="https://activitypub.software/TransFem-org/Sharkey/" external>
+						<FormLink to="https://github.com/ktncode/misskey" external>
 							<template #icon><i class="ph-code ph-bold ph-lg"></i></template>
 							{{ i18n.ts._aboutMisskey.source }} ({{ i18n.ts._aboutMisskey.original_sharkey }})
 							<template #suffix>GitLab</template>
-						</FormLink>
-						<FormLink to="https://opencollective.com/sharkey" external>
-							<template #icon><i class="ph-piggy-bank ph-bold ph-lg"></i></template>
-							{{ i18n.ts._aboutMisskey.donate_sharkey }}
-							<template #suffix>OpenCollective</template>
 						</FormLink>
 					</div>
 				</FormSection>
@@ -132,75 +127,6 @@ const easterEggEmojis = ref<{
 }[]>([]);
 const easterEggEngine = ref<{ stop: () => void } | null>(null);
 const everyone = ref<Section[]>([
-	{
-		heading: i18n.ts._aboutMisskey.projectMembers,
-		link: {
-			label: i18n.ts._aboutMisskey.allContributors,
-			url: 'https://activitypub.software/TransFem-org/Sharkey/-/graphs/develop',
-		},
-		people: fisher_yates([
-			{
-				handle: '@CenTdemeern1',
-				avatar: 'https://secure.gravatar.com/avatar/e97dd57d32caf703cea556ace6304617b7420f17f5b1aac4a1eea8e4234735bb?s=128&d=identicon',
-				link: 'https://activitypub.software/CenTdemeern1',
-			},
-			{
-				handle: '@dakkar',
-				avatar: 'https://secure.gravatar.com/avatar/c71b315eed7c63ff94c42b1b3e8dbad1?s=128&d=identicon',
-				link: 'https://activitypub.software/dakkar',
-			},
-			{
-				handle: '@hazelnoot',
-				avatar: 'https://activitypub.software/uploads/-/system/user/avatar/5/avatar.png?width=128',
-				link: 'https://activitypub.software/fEmber',
-			},
-			{
-				handle: '@julia',
-				avatar: 'https://activitypub.software/uploads/-/system/user/avatar/41/avatar.png?width=128',
-				link: 'https://activitypub.software/julia',
-			},
-			{
-				handle: '@Luna',
-				avatar: 'https://secure.gravatar.com/avatar/4faf37df86a3d93a6c19ed6abf8588eade4efb837410dbbc53021b4fd12eaae7?s=128&d=identicon',
-				link: 'https://activitypub.software/luna',
-			},
-			{
-				handle: '@Marie',
-				avatar: 'https://activitypub.software/uploads/-/system/user/avatar/2/avatar.png?width=128',
-				link: 'https://activitypub.software/marie',
-			},
-			{
-				handle: '@supakaity',
-				avatar: 'https://activitypub.software/uploads/-/system/user/avatar/65/avatar.png?width=128',
-				link: 'https://activitypub.software/supakaity',
-			},
-			{
-				handle: '@tess',
-				avatar: 'https://activitypub.software/uploads/-/system/user/avatar/132/avatar.png?width=128',
-				link: 'https://activitypub.software/tess',
-			},
-		]),
-	},
-	{
-		heading: i18n.ts._aboutMisskey.testers,
-		people: [
-			{
-				handle: '@lucent',
-				avatar: 'https://antani.cyou/proxy/avatar.webp?url=https%3A%2F%2Fantani.cyou%2Ffiles%2Fa2944119-024c-4abd-86e5-64bf0d30b26f&avatar=1',
-				link: 'https://antani.cyou/@lucent',
-			},
-			{
-				handle: '@privateger',
-				avatar: 'https://mediaproxy.plasmatrap.com/?url=https%3A%2F%2Fplasmatrap.com%2Ffiles%2F2cf35a8f-6520-4d4c-9611-bf22ee983293&avatar=1',
-				link: 'https://plasmatrap.com/@privateger',
-			},
-			{
-				handle: '@phoenix_fairy',
-				avatar: 'https://thetransagenda.gay/proxy/avatar.webp?url=https%3A%2F%2Fs3.us-east-005.backblazeb2.com%2Ftranssharkey%2Fnull%2Fd93ac6dc-2020-4b5a-bce7-84b41e97a0ac.png&avatar=1',
-				link: 'https://thetransagenda.gay/@phoenix_fairy',
-			},
-		],
-	},
 	{
 		heading: i18n.ts._aboutMisskey.misskeyContributors,
 		people: [
@@ -283,19 +209,6 @@ await misskeyApi('sponsors', { forceUpdate: false }).then((res) => {
 	}
 	everyone.value.push(section);
 });
-
-/**
- * Based on the pseudocode description from Wikipedia:
- * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
- * Modifies the array in-place, but still returns it for the sake of convenience
- */
-function fisher_yates<T>(array: T[]): T[] {
-	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
-	}
-	return array;
-}
 
 function iconLoaded() {
 	const emojis = store.s.reactions;
